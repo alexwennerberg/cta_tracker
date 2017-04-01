6bb96584e2664d60b6c53238777c3685cdc4d7ce
Alex Wennerberg <alexwennerberg@gmail.com>
2017-03-31 19:35:23 -0500

    added train tracking
    got my CTA train api, app now tracks trains as well as buses

--- cta_tracker.py
+++ cta_tracker.py
@@ -1,26 +1,46 @@
 from tkinter import *
 from tkinter import ttk
 import yaml
-from info_finder import get_times
+from info_finder import get_bus_times, get_train_times
 
-def get_bus_times():
-	output_times = []
+def get_config_data():
 	with open("config.yml") as f:
-		config_data = yaml.load(f)
+		return yaml.load(f)
+
+def print_times():
+	return formatted_bus_times() + formatted_train_times()
+	
+def formatted_bus_times():
+	output_times = []
+	config_data = get_config_data()
 	for stop in config_data["buses"]:
 		route = stop["route"]
 		id = stop["id"]
 		bus_string = (" ".join([str(route), 
 							stop["direction"], 
 							stop["label"], 
-							", ".join(str(x) for x in get_times(route, id))]))
+							", ".join(str(x) for x in get_bus_times(route, id))]))
+		bus_string = bus_string + " minutes"
+		output_times.append(bus_string)
+	return output_times
+
+def formatted_train_times():
+	output_times = []
+	config_data = get_config_data()
+	for stop in config_data["trains"]:
+		station_id = stop["id"]
+		bus_string = (" ".join([stop["route"],
+							stop["label"], 
+							"towards", 
+							stop["direction"], 
+							", ".join(str(x) for x in get_train_times(station_id))]))
 		bus_string = bus_string + " minutes"
 		output_times.append(bus_string)
 	return output_times
 
 if __name__ == "__main__":
 	root = Tk()
-	for i in get_bus_times():	
+	for i in print_times():	
 		w = Label(root, text=i, font=("Helvetica", 30), borderwidth=1, relief="solid")
 		w.pack()
 	root.mainloop()
--- info_finder.py
+++ info_finder.py
@@ -1,6 +1,6 @@
 import requests, json, datetime, yaml
 
-def get_times(route, stop_id):
+def get_bus_times(route, stop_id):
 	time_list = []
 	payload = {"key": yaml.load(open("keys.yml"))["bus"],
 				"format": "json",
@@ -15,10 +15,30 @@ def get_times(route, stop_id):
 		time_list.append(wait_time(prediction["prdtm"], prediction["tmstmp"]))
 	return time_list
 
-def wait_time(prediction_time, current_time):
-	return (format_time(prediction_time) - format_time(current_time)).seconds // 60
+def get_train_times(stop_id):
+	time_list = []
+	payload = {"key": yaml.load(open("keys.yml"))["train"],
+				"outputType": "json",
+				"stpid": stop_id}
+	url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx/"
+	response = json.loads(requests.get(url, params=payload).text)
+	for prediction in response["ctatt"]["eta"]:
+		time_list.append((format_train_time(prediction["arrT"])-format_train_time(prediction["prdt"])).seconds // 60)
+	return time_list
 
-def format_time(time):
+def wait_time(prediction_time, current_time):
+	return (format_bus_time(prediction_time) - format_bus_time(current_time)).seconds // 60
+
+def format_train_time(time):
+	year = int(time[:4])
+	month = int(time[5:7])
+	day = int(time[8:10])
+	hour = int(time[11:13])
+	minute = int(time[14:16])
+	second = int(time[17:19])
+	return datetime.datetime(year, month, day, hour, minute, second)
+
+def format_bus_time(time):
 	year = int(time[:4])
 	month = int(time[4:6])
 	day = int(time[6:8])
